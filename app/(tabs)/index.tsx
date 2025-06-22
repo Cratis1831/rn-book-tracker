cee750c5b54c4f51ecd08957345ada36d4e71987
Cratis1831 <asotoudeh18@hotmail.com>
2025-06-22 08:51:09 -0400

    Add Badge component and enhance Home screen layout
    - Implemented Badge component for displaying messages.
    - Refactored Home screen to include a KeyboardAvoidingView and ScrollView for better user experience.
    - Added TextInput for book title entry and a button to add books.
    - Updated package.json and package-lock.json to include @expo/ui dependency.
    - Modified tsconfig.json to include nativewind-env.d.ts for type definitions.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,55 @@
 import { Stack } from 'expo-router';
+// import { Button, TextInput } from '@expo/ui/swift-ui';
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native';
 
-import { StyleSheet, View } from 'react-native';
-
-import { ScreenContent } from '~/components/ScreenContent';
+// Badge Component
+const Badge = ({ text }: { text: string }) => {
+  return (
+    <View className="mb-4 self-center rounded-full border border-orange-200 bg-orange-50 px-4 py-2">
+      <Text className="text-md self-center font-medium text-[#D97706]">{text}</Text>
+    </View>
+  );
+};
 
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ title: 'Tab One' }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
-      </View>
+      <KeyboardAvoidingView
+        behavior={'padding'}
+        keyboardVerticalOffset={Platform.OS === 'ios' ? 90 : 20}
+        style={{ flex: 1 }}>
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          keyboardShouldPersistTaps="handled"
+          className="flex-1 bg-white">
+          <View className="flex-1 justify-between p-4">
+            <View className="gap-4">
+              <Badge text="Keep track of your books" />
+
+              <TextInput
+                className="w-full self-center rounded-lg border border-gray-300 bg-white px-4 py-2"
+                onChangeText={() => {}}
+                placeholder="Enter book title..."
+              />
+            </View>
+            <TouchableOpacity
+              className="w-full rounded-lg bg-orange-500 py-3"
+              onPress={() => console.log('Pressed')}>
+              <Text className="text-center text-xl font-semibold text-white">Add a Book</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
